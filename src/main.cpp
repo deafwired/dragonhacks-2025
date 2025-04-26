db90870032877ef7046f8699a833b8f35bff601b
TheMightyd7 <dd@davisinfo.com>
2025-04-26 15:48:24 -0400

    Improve serial rate

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,17 @@ void setLCDMessageCentered(String message, int row);
 
 void setup()
 {
-    Serial.begin(9600); // Initialize serial communications with the PC
-    while (!Serial)
-        ;                              // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-    SPI.begin();                       // Init SPI bus
-    mfrc522.PCD_Init();                // Init MFRC522
+    Serial.begin(115200); // Initialize serial communications with the PC
+    // while (!Serial);  // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
     lcd.init();
     lcd.backlight();
-    setLCDMessageCentered("Google VibeCode Studio", 0);
+    setLCDMessageCentered("Starting up...", 0);
+    SPI.begin();                       // Init SPI bus
+    mfrc522.PCD_Init();                // Init MFRC522
     delay(4);                          // Optional delay. Some board do need more time after init to be ready, see Readme
     mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
     Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+    setLCDMessageCentered("Ready to scan", 0);
 }
 
 void loop()
