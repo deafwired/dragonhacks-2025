48935e8c26a87e4359488d450c73dfdbeab5d0a3
Matt <deafwired@proton.me>
2025-04-26 21:28:25 -0400

    this should work but the keyboard no work on linux

--- convert.py
+++ convert.py
@@ -1,14 +1,14 @@
 import serial
-import pynput
+from pynput.keyboard import Controller
 
 MACOS = "/dev/cu.usbmodem2101"
 baudrate = 115200
 
 ser = serial.Serial(MACOS, baudrate)
-keyboard = pynput.keyboard.Controller()
+keyboard = Controller()
 
 while True:
-    data = ser.redaline().decode('utf-8').strip()
+    data = ser.readline().decode('utf-8').strip()
     if "[TYPE]" in data:
         keyboard.type(data[6:])
 
